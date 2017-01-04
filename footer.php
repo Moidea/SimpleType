bf52f67596fbae62c98b413110813ebd86830f8c
Cawn <angellcawn@163.com>
2017-01-04 20:31:45 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
-<div class="footer ofh">Copyright © 2012-<script type="text/javascript">copyright=new Date();update=copyright.getFullYear();document.write(update);</script> <a href="<?php $this->options->siteurl(); ?>"><?php $this->options->title(); ?></a> All Rights Reserved. <font class="copy">Theme <a href="<?php $this->options->siteurl(); ?>">SimpleType.</a></font><span class="time"><script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1259987046'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1259987046%26online%3D2' type='text/javascript'%3E%3C/script%3E"));</script> 网站运行：<?php getBuildTime(); ?></span></div><!-- end #footer -->
+<div class="footer ofh">Copyright © 2012-<script type="text/javascript">copyright=new Date();update=copyright.getFullYear();document.write(update);</script> <a href="<?php $this->options->siteurl(); ?>"><?php $this->options->title(); ?></a> All Rights Reserved. <font class="copy"><?php echo base64_decode('VGhlbWUgPGEgaHJlZj0iaHR0cDovL3d3dy5tb2lkZWEuaW5mbyIgdGFyZ2V0PSJfYmxhbmsiPlNpbXBsZVR5cGUuPC9hPg==');
+?></font><span class="time"><?php $this->options->block01() ?> 网站运行：<?php getBuildTime(); ?></span></div><!-- end #footer -->
 
 <?php $this->footer(); ?>
 
 <section class="float-mobile">
     <!--白天拨打-->
     <div class="pubilc-phone-floatbtn" style="display:-webkit-box">
-        <a class="phone-btn" href="tel:13148454639" style="box-flex:1;-webkit-box-flex:1;"><em class="btn-text">联系我 13148454639</em></a>
+        <a class="phone-btn" href="tel:<?php $this->options->tel() ?>" style="box-flex:1;-webkit-box-flex:1;"><em class="btn-text">联系我 <?php $this->options->tel() ?></em></a>
     </div>
 </section>
 
 </div>
-
 </body>
 </html>
