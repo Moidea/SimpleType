d2f53258abaa957d89f88a75adf11f7a2bbd0c90
Cawn <angellcawn@163.com>
2017-01-04 20:29:33 +0800

    Update post.php

--- post.php
+++ post.php
@@ -1,7 +1,6 @@
 <?php $this->need('header.php'); ?>
 
 <div class="container ofh">
-
 	<div class="content">
 		<div class="post" id="post">
 			<div class="hd">
@@ -21,8 +20,7 @@
 				
 				<br>
 			</div>
-			
-			
+						
 			<div class="element">
 			<?php if(isset($this->fields->copyright)){ ?>
 				<div class="copyright">
@@ -33,16 +31,9 @@
 				<div style="text-align: center;"><b>赞 助 支 持</b></div>
 				<div align="center"><img class="wp-image-558 size-thumbnail" src="<?php $this->options->weixin() ?>" alt="pay_weixin" width="150" height="150"/><img class="wp-image-558 size-thumbnail" src="<?php $this->options->alipay() ?>" alt="pay_weixin" width="150" height="150" /></div>
 				<!-- END donate by moidea.info -->
-			<?php } ?>
-			
-			
-			<div class="tags fl">标签:<?php $this->tags(' ', true, 'none'); ?></div><!--百度分享-->
-	<div class="bdsharebuttonbox"><a href="#" class="bds_more" data-cmd="more"></a><a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a><a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a><a href="#" class="bds_tqq" data-cmd="tqq" title="分享到腾讯微博"></a><a href="#" class="bds_renren" data-cmd="renren" title="分享到人人网"></a><a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信"></a><a href="#" class="bds_sqq" data-cmd="sqq" title="分享到QQ好友"></a><a href="#" class="bds_huaban" data-cmd="huaban" title="分享到花瓣"></a><a href="#" class="bds_ty" data-cmd="ty" title="分享到天涯社区"></a><a href="#" class="bds_copy" data-cmd="copy" title="分享到复制网址"></a></div>
-	<script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"16"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
-	<!--百度分享-->
-			
-			<br>
-	
+			<?php } ?>						
+			<div class="tags fl">标签:<?php $this->tags(' ', true, 'none'); ?></div>			
+			<br>	
 			</div>
 		</div>
 	</div>
@@ -67,10 +58,6 @@
 	</div>	
 	
 	<?php $this->need('comments.php'); ?>
-
-
 </div>
 
-
-	
 <?php $this->need('footer.php'); ?>
